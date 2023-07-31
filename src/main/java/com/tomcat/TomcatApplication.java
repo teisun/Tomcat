fbcff9fdfafaee4e3c3e0b62b7b051386b50f77a
tomcat <lilongtao@sunmi.com>
2023-07-31 16:32:34 +0800

    feat: 拷贝chatgpt-stream-ouput代码初始化项目

---
+++ src/main/java/com/tomcat/TomcatApplication.java
@@ -0,0 +1,54 @@
+package com.tomcat;
+
+import com.unfbx.chatgpt.OpenAiStreamClient;
+import com.unfbx.chatgpt.function.KeyRandomStrategy;
+import com.unfbx.chatgpt.interceptor.OpenAILogger;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class TomcatApplication {
+
+    @Value("${chatgpt.apiKey}")
+    private List<String> apiKey;
+    @Value("${chatgpt.apiHost}")
+    private String apiHost;
+
+    public static void main(String[] args) {
+        System.out.println("Hello Tomcat!");
+        SpringApplication.run(TomcatApplication.class, args);
+    }
+
+    @Bean
+    public OpenAiStreamClient openAiStreamClient() {
+        //本地开发需要配置代理地址
+//        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7890));
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new OpenAILogger());
+        //!!!!!!测试或者发布到服务器千万不要配置Level == BODY!!!!
+        //!!!!!!测试或者发布到服务器千万不要配置Level == BODY!!!!
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+        OkHttpClient okHttpClient = new OkHttpClient
+                .Builder()
+//                .proxy(proxy)
+                .addInterceptor(httpLoggingInterceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(600, TimeUnit.SECONDS)
+                .readTimeout(600, TimeUnit.SECONDS)
+                .build();
+        return OpenAiStreamClient
+                .builder()
+                .apiHost(apiHost)
+                .apiKey(apiKey)
+                //自定义key使用策略 默认随机策略
+                .keyStrategy(new KeyRandomStrategy())
+                .okHttpClient(okHttpClient)
+                .build();
+    }
+}
---
+++ src/main/java/com/tomcat/entity/Chat.java
@@ -0,0 +1,19 @@
+package com.tomcat.entity;
+
+import com.unfbx.chatgpt.entity.chat.Message;
+import lombok.Data;
+
+import java.util.List;
+/**
+ * 描述：
+ *
+ * @author https:www.unfbx.com
+ * @date 2023-04-10
+ */
+@Data
+public class Chat {
+
+    private String uid;
+
+    private List<Message> message;
+}
